d348debd98c0ba60da6a097ba918868a1e704203
saanviagiwal <148381590+saanviagiwal@users.noreply.github.com>
2025-08-11 08:36:27 -0400

    Update OperatorsLab.java

--- Day 1/OperatorsLab.java
+++ Day 1/OperatorsLab.java
@@ -1,6 +1,7 @@
 public class OperatorsLab {
     public static void main(String[] args) {
         // Create two int variables a and b
+        //Example, int z = 30;
         
         // Print their sum, difference, product, quotient, and remainder
         
