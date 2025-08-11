24f999e2cb77b253a279ee22e7f373502a1adec0
saanviagiwal <148381590+saanviagiwal@users.noreply.github.com>
2025-08-11 02:39:36 -0400

    Update VariablesLab.java

--- Day 1/VariablesLab.java
+++ Day 1/VariablesLab.java
@@ -1,12 +1,12 @@
 public class VariablesLab {
     public static void main(String[] args) {
-        // TODO: Create an int variable called number and set it to 100
+        // Create an int variable called number and set it to 100
         
-        // TODO: Print the variable
+        // Print the variable
         
-        // TODO: Create one variable for each of these types:
+        // Create one variable for each of these types:
         // short, long, byte, float, double, boolean, char, String
         
-        // TODO: Print each one
+        // Print each one
     }
 }
