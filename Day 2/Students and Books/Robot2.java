af7fa2fd4bd334b12711e526e5c42065a7c337e3
saanviagiwal <148381590+saanviagiwal@users.noreply.github.com>
2025-08-11 02:30:49 -0400

    Create Robot2.java

---
+++ Day 2/Students and Books/Robot2.java
@@ -0,0 +1,10 @@
+class Robot2 {
+    String name;
+    String item;
+
+    // TODO: Create a constructor to set name and set item to null
+    
+    // TODO: Create a method pickUp(String newItem) that stores the item and prints a message
+    
+    // TODO: Create a method drop() that prints a message and sets item to null
+}
