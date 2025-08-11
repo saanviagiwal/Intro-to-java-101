bfcaae0fd5f68c3a1caed8e0742a19deb0afa3e9
saanviagiwal <148381590+saanviagiwal@users.noreply.github.com>
2025-08-11 02:29:45 -0400

    Create Robot.java

---
+++ Day 2/Hello Robot/Robot.java
@@ -0,0 +1,13 @@
+// Robot.java
+class Robot {
+    String name;
+    int x, y;
+
+    // TODO: Create a constructor that sets name, x, and y
+    
+    // TODO: Create a method moveForward() that increases y by 1 and prints the new location
+    
+    // TODO: Create a method turnLeft() that prints that the robot turned left
+    
+    // TODO: Create a method putBeeper() that prints that the robot put a beeper at its location
+}
