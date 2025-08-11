e6eb4c62419d8da894a556d87ba1f1a4b70da93c
saanviagiwal <148381590+saanviagiwal@users.noreply.github.com>
2025-08-11 02:29:24 -0400

    Update and rename Day 2/Lab 1 — Hello, Robot! to Day 2/Hello Robot/HelloRobot.java

---
+++ Day 2/Hello Robot/HelloRobot.java
@@ -0,0 +1,11 @@
+
+// HelloRobot.java
+public class HelloRobot {
+    public static void main(String[] args) {
+        // TODO: Create a Robot named "Karel" starting at (0, 0)
+        
+        // TODO: Call methods to move, put beeper, move again, turn left
+        
+        // BONUS: Change starting position
+    }
+}
