aa365d487c79162cc12b38280db1a913300de65f
saanviagiwal <148381590+saanviagiwal@users.noreply.github.com>
2025-08-11 02:46:31 -0400

    Update LoopsLab.java

--- Day 4/LoopsLab.java
+++ Day 4/LoopsLab.java
@@ -1,7 +1,7 @@
 public class LoopsLab {
     public static void main(String[] args) {
-        // TODO: Write a for loop printing 0 to 9
+        // Write a for loop printing 0 to 9
         
-        // TODO: Write a while loop printing 0 to 9
+        // Write a while loop printing 0 to 9
     }
 }
