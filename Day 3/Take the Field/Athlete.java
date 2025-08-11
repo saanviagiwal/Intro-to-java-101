c74bfdc81cd4ebc792cb39a9880899c323a6417f
saanviagiwal <148381590+saanviagiwal@users.noreply.github.com>
2025-08-11 02:33:47 -0400

    Update and rename Day 3/Lab 2 — Take the Field to Day 3/Take the Field/Athlete.java

---
+++ Day 3/Take the Field/Athlete.java
@@ -0,0 +1,13 @@
+// Athlete.java
+public class Athlete {
+    int x, y;
+    String name;
+    
+    // TODO: Add a no-arg constructor that calls another constructor
+    
+    // TODO: Add a constructor with name, x, y
+    
+    public void reportPosition() {
+        // TODO: Print name and position
+    }
+}
