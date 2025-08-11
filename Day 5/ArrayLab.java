53e7b64ce3d80d10be33242f06088f6307692752
saanviagiwal <148381590+saanviagiwal@users.noreply.github.com>
2025-08-11 02:47:37 -0400

    Update ArrayLab.java

--- Day 5/ArrayLab.java
+++ Day 5/ArrayLab.java
@@ -5,12 +5,12 @@ public class ArrayLab {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        // TODO: Ask for array length
+        // Ask for array length
         
-        // TODO: Create int[] of given length
+        // Create int[] of given length
         
-        // TODO: Fill with ascending numbers starting at 0
+        // Fill with ascending numbers starting at 0
         
-        // TODO: Print array using Arrays.toString()
+        // Print array using Arrays.toString()
     }
 }
