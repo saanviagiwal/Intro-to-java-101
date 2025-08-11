82133a34a8fb1245a4ee41bcc626b8e70a01e02c
saanviagiwal <148381590+saanviagiwal@users.noreply.github.com>
2025-08-11 02:34:04 -0400

    Create Lab09.java

---
+++ Day 3/Take the Field/Lab09.java
@@ -0,0 +1,14 @@
+// Lab09.java
+public class Lab09 {
+    public static void takeTheField(Athlete a) {
+        // TODO: Set athlete position to (5, 5) and call reportPosition
+    }
+    
+    public static void main(String[] args) {
+        // TODO: Create an array of 6 Athlete objects using default constructor
+        
+        // TODO: Create a coach using the 3-arg constructor
+        
+        // TODO: Use takeTheField on each player
+    }
+}
