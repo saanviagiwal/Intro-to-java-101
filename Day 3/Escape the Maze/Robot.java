fccb33023c037a14868e9194f1194707d0731db7
saanviagiwal <148381590+saanviagiwal@users.noreply.github.com>
2025-08-11 02:32:50 -0400

    Create Robot.java

---
+++ Day 3/Escape the Maze/Robot.java
@@ -0,0 +1,12 @@
+// Robot.java
+public class Robot {
+    int x = 0, y = 0;
+    
+    public void move() {
+        // TODO: Increase y by 1 and print new location
+    }
+    
+    public void turnLeft() {
+        // TODO: Print that robot turned left
+    }
+}
