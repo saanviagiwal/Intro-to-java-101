ba358c6d31fb666b64321190d89428eb48cd32ef
saanviagiwal <148381590+saanviagiwal@users.noreply.github.com>
2025-08-11 02:32:32 -0400

    Update and rename Day 3/Lab 1 — Escape the Maze to Day 3/Escape the Maze/Lab08.java

---
+++ Day 3/Escape the Maze/Lab08.java
@@ -0,0 +1,8 @@
+// Lab08.java
+public class Lab08 {
+    public static void main(String[] args) {
+        // TODO: Create an Athlete
+        
+        // TODO: Move through a maze and mark path
+    }
+}
