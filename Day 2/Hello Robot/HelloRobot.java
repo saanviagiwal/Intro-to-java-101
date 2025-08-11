6bfc09b686d3c317aa925307e01c7908786d4009
saanviagiwal <148381590+saanviagiwal@users.noreply.github.com>
2025-08-11 02:41:11 -0400

    Update HelloRobot.java

--- Day 2/Hello Robot/HelloRobot.java
+++ Day 2/Hello Robot/HelloRobot.java
@@ -2,10 +2,10 @@
 // HelloRobot.java
 public class HelloRobot {
     public static void main(String[] args) {
-        // TODO: Create a Robot named "Karel" starting at (0, 0)
+        // Create a Robot named "Karel" starting at (0, 0)
         
-        // TODO: Call methods to move, put beeper, move again, turn left
+        // Call methods to move, put beeper, move again, turn left
         
-        // BONUS: Change starting position
+        // Change starting position
     }
 }
