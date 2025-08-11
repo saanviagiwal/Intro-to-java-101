8c7ee8e46206875ee37e48350d0c9b6b088c6b85
saanviagiwal <148381590+saanviagiwal@users.noreply.github.com>
2025-08-11 02:41:37 -0400

    Update Robot.java

--- Day 2/Hello Robot/Robot.java
+++ Day 2/Hello Robot/Robot.java
@@ -3,11 +3,11 @@ class Robot {
     String name;
     int x, y;
 
-    // TODO: Create a constructor that sets name, x, and y
+    // Create a constructor that sets name, x, and y
     
-    // TODO: Create a method moveForward() that increases y by 1 and prints the new location
+    // Create a method moveForward() that increases y by 1 and prints the new location
     
-    // TODO: Create a method turnLeft() that prints that the robot turned left
+    // Create a method turnLeft() that prints that the robot turned left
     
-    // TODO: Create a method putBeeper() that prints that the robot put a beeper at its location
+    // Create a method putBeeper() that prints that the robot put a beeper at its location
 }
