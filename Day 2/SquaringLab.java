2853e4c9ab5c837b2bfb04440bf382913d9b9fe2
saanviagiwal <148381590+saanviagiwal@users.noreply.github.com>
2025-08-11 02:43:18 -0400

    Update SquaringLab.java

--- Day 2/SquaringLab.java
+++ Day 2/SquaringLab.java
@@ -2,10 +2,10 @@ public class SquaringLab {
     // TODO: Write a method square(int x) that returns x times x
     
     public static void main(String[] args) {
-        // TODO: Store a number in a variable
+        // Store a number in a variable
         
-        // TODO: Call square() and print the result
+        // Call square() and print the result
         
-        // BONUS: Write a method exp(double base, double exponent) using Math.pow
+        // Write a method exp(double base, double exponent) using Math.pow
     }
 }
