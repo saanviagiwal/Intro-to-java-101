a77495970ad58725ebf0244944f0267c68c649d6
saanviagiwal <148381590+saanviagiwal@users.noreply.github.com>
2025-08-11 02:33:06 -0400

    Create  Athlete.java

---
+++ Day 3/Escape the Maze/Athlete.java
@@ -0,0 +1,14 @@
+// Athlete.java
+public class Athlete extends Robot {
+    public void turnRight() {
+        // TODO: Use turnLeft() 3 times
+    }
+    
+    public void turnAround() {
+        // TODO: Use turnLeft() 2 times
+    }
+    
+    public void putAndMove() {
+        // TODO: Print that a marker was placed, then move
+    }
+}
