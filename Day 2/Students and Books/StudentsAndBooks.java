a22f46010cda6ae31c660a85abb9c90605e8d5df
saanviagiwal <148381590+saanviagiwal@users.noreply.github.com>
2025-08-11 02:42:36 -0400

    Update StudentsAndBooks.java

--- Day 2/Students and Books/StudentsAndBooks.java
+++ Day 2/Students and Books/StudentsAndBooks.java
@@ -1,12 +1,12 @@
 
 public class StudentsAndBooks {
     public static void main(String[] args) {
-        // TODO: Create two Robot objects named Lisa and Pete
+        // Create two Robot objects named Lisa and Pete
         
-        // TODO: Make Lisa pick up a book, Pete pick up two books
+        // Make Lisa pick up a book, Pete pick up two books
         
-        // TODO: Make Lisa drop her book and Pete pick it up
+        // Make Lisa drop her book and Pete pick it up
         
-        // BONUS: Add a method to "walk" to a location
+        // Add a method to "walk" to a location
     }
 }
