af8e1eda7dcb1eadf415cdb39ac3b7c619f9d973
saanviagiwal <148381590+saanviagiwal@users.noreply.github.com>
2025-08-11 02:46:50 -0400

    Update Quiz.java

--- Day 4/Quiz.java
+++ Day 4/Quiz.java
@@ -4,10 +4,10 @@ public class Quiz {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        String[] questions = {"2+2?", "Capital of France?", /* TODO: Add 3 more */};
-        String[] answers = {"4", "Paris", /* TODO: Add matching answers */};
+        String[] questions = {"2+2?", "Capital of France?", /* Add 3 more */};
+        String[] answers = {"4", "Paris", /* Add matching answers */};
         
-        // TODO: Loop through questions
+        // Loop through questions
         // If correct, move to next
         // If wrong, start over
     }
