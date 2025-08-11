0d4501cd276017fe41f48e16cb8ce8b0953d8c58
saanviagiwal <148381590+saanviagiwal@users.noreply.github.com>
2025-08-11 02:35:30 -0400

    Create PolymorphismLab.java

---
+++ Day 3/Polymorphism Sandbox/PolymorphismLab.java
@@ -0,0 +1,8 @@
+// PolymorphismLab.java
+public class PolymorphismLab {
+    public static void main(String[] args) {
+        // TODO: Create Animal, Dog, and Lab objects as Animal type variables
+        
+        // TODO: Call sound() on each
+    }
+}
