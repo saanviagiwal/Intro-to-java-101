59f9ed47b21ddf2b05afb4125068ced4ce4cbbe4
saanviagiwal <148381590+saanviagiwal@users.noreply.github.com>
2025-08-11 02:44:00 -0400

    Update Lab08.java

--- Day 3/Escape the Maze/Lab08.java
+++ Day 3/Escape the Maze/Lab08.java
@@ -1,8 +1,8 @@
 // Lab08.java
 public class Lab08 {
     public static void main(String[] args) {
-        // TODO: Create an Athlete
+        // Create an Athlete
         
-        // TODO: Move through a maze and mark path
+        // Move through a maze and mark path
     }
 }
